48d6ba32965f194a7cc8f37f24b96d58ee526a78
ting <ting@qq.com>
2021-05-13 01:41:29 +0800

    ting第一次提交代码

---
+++ 01-git代码的测试/src/com/gec/aa/DemoTest1.java
@@ -0,0 +1,11 @@
+package com.gec.aa;
+
+public class DemoTest1 {
+    public static void main(String[] args) {
+
+
+        System.out.println("haley");
+        System.out.println("penny");
+        System.out.println("abigail");
+    }
+}
