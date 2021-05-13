5a630cc853699581cae80a8776d9bb0668bd2637
ting <ting@qq.com>
2021-05-13 09:25:33 +0800

    ting第1次提交主方法

---
+++ 01-git代码的测试/src/com/gec/aa/Test1.java
@@ -0,0 +1,11 @@
+package com.gec.aa;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("11111");
+        System.out.println("22222");
+        System.out.println("33333");
+    }
+}
